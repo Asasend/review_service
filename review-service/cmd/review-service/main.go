8a96591f2cc03b3733f46fe8903e1a60f08f5e81
Asasend <2747217535@qq.com>
2025-08-26 01:04:12 +0800

    consul服务注册

--- review-service/cmd/review-service/main.go
+++ review-service/cmd/review-service/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"os"
 
+	"github.com/go-kratos/kratos/v2/registry"
+
 	"review-service/internal/conf"
 	"review-service/pkg/snowflake"
 
@@ -21,9 +23,9 @@ import (
 // go build -ldflags "-X main.Version=x.y.z"
 var (
 	// Name is the name of the compiled software.
-	Name string
+	Name string = "review.service"
 	// Version is the version of the compiled software.
-	Version string
+	Version string = "v0.1"
 	// flagconf is the config flag.
 	flagconf string
 
@@ -34,7 +36,7 @@ func init() {
 	flag.StringVar(&flagconf, "conf", "../../configs", "config path, eg: -conf config.yaml")
 }
 
-func newApp(logger log.Logger, gs *grpc.Server, hs *http.Server) *kratos.App {
+func newApp(logger log.Logger, r registry.Registrar, gs *grpc.Server, hs *http.Server) *kratos.App {
 	return kratos.New(
 		kratos.ID(id),
 		kratos.Name(Name),
@@ -45,6 +47,7 @@ func newApp(logger log.Logger, gs *grpc.Server, hs *http.Server) *kratos.App {
 			gs,
 			hs,
 		),
+		kratos.Registrar(r),
 	)
 }
 
@@ -75,7 +78,13 @@ func main() {
 		panic(err)
 	}
 
-	app, cleanup, err := wireApp(bc.Server, bc.Data, logger)
+	// 对应的是解析registry.yaml中的配置
+	var rc conf.Registry
+	if err := c.Scan(&rc); err != nil {
+		panic(err)
+	}
+
+	app, cleanup, err := wireApp(bc.Server, &rc, bc.Data, logger)
 	if err != nil {
 		panic(err)
 	}
